056acef3a7228a3dd6ff56c38167ff865512629b
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-09-15 04:25:26 +0500

    Task4

---
+++ Tyuiu.EvdokimovKP.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.EvdokimovKP.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.EvdokimovKP.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("740df083-ecf1-42d4-a617-2738b33c5c1c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.EvdokimovKP.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.EvdokimovKP.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.EvdokimovKP.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пример линейной структуры программы
+            //Вызов метода сложения Addition
+            Console.WriteLine(DataService.Addition(1, 5));
+            //Вызов метода сложения Substraction
+            Console.WriteLine(DataService.Substraction(15, 5));
+            //Вызов метода сложения Multiplication
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            //Вызов метода сложения Division
+            Console.WriteLine(DataService.Division(5, 5));
+
+            Console.ReadKey();
+        }
+    }
+}
